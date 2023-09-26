242374869ff57584dad41eb05dc9fa904976df24
sarangp323 <sarangp323@gmail.com>
2023-09-26 14:25:33 +0530

    added model recipe and its apis
    Signed-off-by: sarangp323 <sarangp323@gmail.com>

---
+++ app/recipe/serializers.py
@@ -0,0 +1,22 @@
+"""
+Serializers for recipe APIS.
+"""
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializers for recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail review"""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
---
+++ app/recipe/urls.py
@@ -0,0 +1,17 @@
+"""
+URL mapping for recipe api.
+"""
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
